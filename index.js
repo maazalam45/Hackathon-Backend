273a0d0dcf3fa9259b92028b0e255365bc56a52a
MaazAlam <maazkaalam@gmail.com>
2025-01-25 17:14:19 +0500

    PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(cors());
